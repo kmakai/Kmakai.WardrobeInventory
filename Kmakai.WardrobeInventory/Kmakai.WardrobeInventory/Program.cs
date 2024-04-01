60a0e7b0ccf0520241f5dca3a9792c3a205a3001
kmakai <makaikwag@gmail.com>
2024-04-01 18:49:57 -0400

    setup db, page added

--- Kmakai.WardrobeInventory/Kmakai.WardrobeInventory/Program.cs
+++ Kmakai.WardrobeInventory/Kmakai.WardrobeInventory/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddRazorComponents()
 builder.Services.AddDbContext<WardrobeContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("WardrobeContext")));
 
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
