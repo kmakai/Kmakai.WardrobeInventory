d31d03ec22bcf166f9e7ae9c1766b4e61034b46e
kmakai <makaikwag@gmail.com>
2024-04-03 18:33:49 -0400

    working on wardrobe error

--- Shared/Models/Wardrobe.cs
+++ Shared/Models/Wardrobe.cs
@@ -5,6 +5,9 @@ public class Wardrobe
     public int Id { get; set; }
     public string? Name { get; set; }
 
+    public int? TopId { get; set; }
+    public int? BottomId { get; set; }
+    public int? FootwearId { get; set; }
     public WardrobeItem? Top { get; set;}
     public WardrobeItem? Bottom { get; set; }
     public WardrobeItem? Footwear { get; set; }
